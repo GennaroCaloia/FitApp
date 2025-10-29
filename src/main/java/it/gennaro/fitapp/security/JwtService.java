01129667e06eab15154825c0e766a61f5f6de1c5
Gennaro Caloia <gennarocaloia@gmail.com>
2025-10-29 12:36:23 +0100

    Aggiunti in JwtService metodi isValid, getSigningKey e parseClaims (in ottica test)

--- src/main/java/it/gennaro/fitapp/security/JwtService.java
+++ src/main/java/it/gennaro/fitapp/security/JwtService.java
@@ -1,7 +1,6 @@
 package it.gennaro.fitapp.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import it.gennaro.fitapp.config.JwtProperties;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
 
@@ -39,6 +39,11 @@ public class JwtService {
                 .compact();
     }
 
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(properties.getSecret());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
     public String extractUsername(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(secretKey)
@@ -47,4 +52,22 @@ public class JwtService {
                 .getBody()
                 .getSubject();
     }
+
+    public boolean isValid(String token) {
+        try {
+            var claims = parseClaims(token).getBody();
+            return claims.getExpiration().after(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private Jws<Claims> parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .requireIssuer(properties.getIssuer())
+                .build()
+                .parseClaimsJws(token);
+    }
+
 }
