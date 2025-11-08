d39d1bae5221b110006a9e34d677e0dc86d72368
Gennaro Caloia <gennarocaloia@gmail.com>
2025-11-08 17:05:04 +0100

    Aggiunto findByIdAndOwner per WorkoutPlanService

--- src/main/java/it/gennaro/fitapp/repository/PlanRepository.java
+++ src/main/java/it/gennaro/fitapp/repository/PlanRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.*;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
     Page<Plan> findByOwner(User owner, Pageable pageable);
 
+    Optional<Plan> findByIdAndOwner(Long id, User owner);
 }
